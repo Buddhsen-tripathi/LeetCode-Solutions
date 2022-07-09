8481b46f4cdc4aabbda97f59c1022ceb81494d5f
Buddhsen Tripathi <buddhsent19@gmail.com>
2022-07-09 22:19:08 +0530

    Added solution - LeetHub

---
+++ Print Diagonally - GFG/print-diagonally.java
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            
+            int matrix[][] = new int[n][n];
+            
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                 matrix[i][j] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            ArrayList<Integer> ans = ob.downwardDigonal(n, matrix);
+            for (Integer val: ans) 
+                System.out.print(val+" "); 
+            System.out.println();
+        }
+    }
+}// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static ArrayList<Integer> downwardDigonal(int n, int A[][])
+    {
+        // code here 
+       ArrayList<Integer> arr=new ArrayList<Integer>();
+       for(int k=0;k<n;k++)
+       {
+           int j=k;
+           int i=0;
+           while(j>=0 &&i<n)
+           {
+               arr.add(A[i][j]);
+               i++;
+               j--;
+           }
+       }
+       for(int k=1;k<n;k++)
+       {   int j=n-1;
+           int i=k;
+           while(j>=1 && i<n)
+           {arr.add(A[i][j]);
+               i++;
+               j--;
+
+           }    
+       }
+       return arr;
+    }
+}
