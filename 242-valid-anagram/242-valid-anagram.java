ce72669f08076a9755b2c5df5e9e640c76e3ff3b
Buddhsen Tripathi <buddhsent19@gmail.com>
2022-07-28 07:59:44 +0530

    Time: 6 ms (63.45%), Space: 45.3 MB (30.70%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] c1 = s.toCharArray();
+        char[] c2 = t.toCharArray();
+        
+        
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        
+        return (Arrays.equals(c1,c2));
+    }
+}
