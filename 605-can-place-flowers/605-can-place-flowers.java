c7c3763861c90d85fcf4b5a9f4ec560059cc3fce
Buddhsen Tripathi <buddhsent19@gmail.com>
2025-01-03 19:22:46 +0530

    605-can-place-flowers

---
+++ 605-can-place-flowers/605-can-place-flowers.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        int length = flowerbed.length;
+
+        for (int i = 0; i < length; i++) {
+
+            if (flowerbed[i] == 0 &&
+                (i == 0 || flowerbed[i - 1] == 0) &&
+                (i == length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                count++; 
+
+                if (count >= n) {
+                    return true;
+                }
+            }
+        }
+
+        return count >= n;
+    }
+}
